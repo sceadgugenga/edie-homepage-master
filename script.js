30440d49ac7f994317cdbcf0950d373589034c3e
Sceadgugenga <sceadgugenga@gmail.com>
2022-12-10 21:36:36 -0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+hamburger.addEventListener("click", toggleMenu, false);
+window.addEventListener("resize", checkWindowSize);
+
+let newBreakpoint = true;
+
+function checkWindowSize() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 900) {
+    if (newBreakpoint) {
+      let mainNav = document.getElementById("main-nav");
+      mainNav.classList.remove("menu-open");
+
+      hamburger.classList.remove("menu-open-icon");
+      newBreakpoint = false;
+    } else {
+      newBreakpoint = true;
+    }
+  }
+}
+
+function toggleMenu() {
+  let mainNav = document.getElementById("main-nav");
+
+  let mainNavClassList = mainNav.className;
+  mainNav.classList.toggle("menu-open");
+  hamburger.classList.toggle("menu-open-icon");
+}
